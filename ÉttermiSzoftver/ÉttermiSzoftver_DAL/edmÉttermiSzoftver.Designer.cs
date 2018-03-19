c3eedacfe2e93954109a9c38a27195251c468583
szepbalazs06 <szepbalazs06@gmail.com>
2018-03-19 11:03:25 +0100

    enTipus név hozzáadva

--- ÉttermiSzoftver/ÉttermiSzoftver_DAL/edmÉttermiSzoftver.Designer.cs
+++ ÉttermiSzoftver/ÉttermiSzoftver_DAL/edmÉttermiSzoftver.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Hw6-LW\Desktop\ÉttermiSzoftver\ÉttermiSzoftver_DAL\edmÉttermiSzoftver.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Hw6-LW\Source\Repos\Ettermi-Szoftver\ÉttermiSzoftver\ÉttermiSzoftver_DAL\edmÉttermiSzoftver.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
